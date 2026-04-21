dfc2ffb1a2a07cc6af94c05a46f894f3c50ed05c
zer0sanity <brian.rittner@gmail.com>
2026-04-20 19:54:21 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zmk-nix,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    in
+    {
+      packages = forAllSystems (system: rec {
+        default = firmware;
+
+        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          name = "firmware";
+
+          src = nixpkgs.lib.sourceFilesBySuffices self [
+            ".board"
+            ".cmake"
+            ".conf"
+            ".defconfig"
+            ".dts"
+            ".dtsi"
+            ".json"
+            ".keymap"
+            ".overlay"
+            ".shield"
+            ".yml"
+            "_defconfig"
+          ];
+
+          board = "kinesis_adv360_pro_%PART%";
+          shield = "adv360_pro_%PART%";
+
+          zephyrDepsHash = "sha256-13db6bDxuRDkXAGhSKx2Q/Lm9q7xfDs1kwER0PNJMVs=";
+
+          meta = {
+            description = "ZMK firmware";
+            license = nixpkgs.lib.licenses.mit;
+            platforms = nixpkgs.lib.platforms.all;
+          };
+        };
+
+        flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+        update = zmk-nix.packages.${system}.update;
+      });
+
+      devShells = forAllSystems (system: {
+        default = zmk-nix.devShells.${system}.default;
+      });
+    };
+}
+
+# Local Variables:
+# jinx-local-words: "cmake conf defconfig dts dtsi json keymap kinesis lilyinstarlight yml zmk"
+# End:
